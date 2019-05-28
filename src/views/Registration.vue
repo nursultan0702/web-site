e8cfb74df8a262265f4680503b0b331afbd6dd86
nursultan0702 <nursultan.sadyk1995@gmail.com>
2019-05-28 13:44:12 +0600

    deleted register disable

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -33,7 +33,7 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" @click.prevent="registration" :disabled="processing || valid">Зарегистрироваться</v-btn>
+            <v-btn color="primary" @click.prevent="registration">Зарегистрироваться</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
